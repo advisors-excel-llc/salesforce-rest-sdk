46a9c643550f5cbe3c21c19fb92b6630b375d339
Alex Boyce <curiosity26@gmail.com>
2018-10-31 10:53:57 -0400

    Fix issue with line breaks in a csv column (#19)
    * Fix issue with line breaks in a csv column
    
    * Ensure line breaks are removed from the end of the read line. Leaving them will cause issues with str_getcsv

---
+++ Tests/Psr7/CsvStreamTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex.boyce
+ * Date: 10/31/18
+ * Time: 10:12 AM
+ */
+
+namespace AE\SalesforceRestSdk\Tests\Psr7;
+
+use AE\SalesforceRestSdk\Psr7\CsvStream;
+use function GuzzleHttp\Psr7\stream_for;
+use PHPUnit\Framework\TestCase;
+
+class CsvStreamTest extends TestCase
+{
+    public function testRead()
+    {
+        $testLine = '"Item 1","Item 2","This is a longer'.PHP_EOL.'multiline field","Last Field"'.PHP_EOL;
+        $stream = stream_for($testLine);
+        $csv = new CsvStream($stream);
+
+        $line = CsvStream::readline($stream);
+
+        $this->assertEquals(rtrim($testLine, "\n"), $line);
+
+        $stream->rewind();
+
+        $row = $csv->read();
+
+        $this->assertEquals(
+            [
+                'Item 1',
+                'Item 2',
+                'This is a longer'.PHP_EOL.'multiline field',
+                'Last Field'
+            ],
+            $row
+        );
+    }
+}
