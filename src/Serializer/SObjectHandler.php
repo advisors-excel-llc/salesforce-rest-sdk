1f2e4a439eb2b5e4443cf279a5b090793a2ab5a3
Nicholas Lafrance <nickalafrance@gmail.com>
2020-04-07 12:10:53 -0500

    Updating the serializer and deserializer.

--- src/Serializer/SObjectHandler.php
+++ src/Serializer/SObjectHandler.php
@@ -8,12 +8,16 @@
 
 namespace AE\SalesforceRestSdk\Serializer;
 
+use AE\SalesforceRestSdk\Model\Rest\Composite\CompositeCollection;
 use AE\SalesforceRestSdk\Model\SObject;
 use JMS\Serializer\Context;
 use JMS\Serializer\DeserializationContext;
+use JMS\Serializer\GraphNavigator;
 use JMS\Serializer\Handler\SubscribingHandlerInterface;
 use JMS\Serializer\JsonDeserializationVisitor;
 use JMS\Serializer\JsonSerializationVisitor;
+use JMS\Serializer\Metadata\ClassMetadata;
+use JMS\Serializer\Metadata\StaticPropertyMetadata;
 
 class SObjectHandler implements SubscribingHandlerInterface
 {
@@ -21,9 +25,17 @@ class SObjectHandler implements SubscribingHandlerInterface
     {
         return [
             [
-                'type'   => SObject::class,
+                'direction' => GraphNavigator::DIRECTION_SERIALIZATION,
+                'type' => SObject::class,
                 'format' => 'json',
+                'method' => 'serializeSObjectTojson'
             ],
+            [
+                'direction' => GraphNavigator::DIRECTION_DESERIALIZATION,
+                'type' => SObject::class,
+                'format' => 'json',
+                'method' => 'deserializeSObjectFromjson'
+            ]
         ];
     }
 
@@ -35,51 +47,41 @@ class SObjectHandler implements SubscribingHandlerInterface
     ): array {
         $object = [];
 
-        if (null === $visitor->getRoot()) {
-            $visitor->setRoot(['__replaceme__' => $object]);
-        }
-
         foreach ($sobject->getFields() as $field => $value) {
             if (null === $value) {
                 continue;
             }
-
             if (is_object($value)) {
-                $className = get_class($value);
-                if (false !== $className) {
-                    if (\DateTime::class === $className) {
-                        $object[$field] = $context->getNavigator()->accept(
-                            $value,
-                            ['name' => 'DateTime', 'params' => [\DATE_ISO8601, 'UTC']],
-                            $context
-                        )
-                        ;
-                    } elseif (\DateTimeImmutable::class === $className) {
-                        $object[$field] = $context->getNavigator()->accept(
-                            $value,
-                            ['name' => 'DateTimeImmutable', 'params' => [\DATE_ISO8601, 'UTC']],
-                            $context
-                        )
-                        ;
-                    } else {
-                        $object[$field] = $context->getNavigator()->accept(
-                            $value,
-                            ['name' => $className],
-                            $context
-                        )
-                        ;
-                    }
-                } else {
-                    $object[$field] = $value;
+                $class = get_class($value);
+                $classMetadata = new ClassMetadata($class);
+                $visitor->startVisitingObject(
+                    $classMetadata,
+                    $value,
+                    []
+                );
+                switch ($class) {
+                    case \DateTime::class:
+                    case \DateTimeImmutable::class:
+                        $dateMeta = new StaticPropertyMetadata(\DateTime::class, $field, $value);
+                        $dateMeta->setType(['name' => 'DateTime', 'params' => [\DATE_ISO8601, 'UTC']]);
+                        $visitor->visitProperty($dateMeta, $value);
+                        break;
+                    default:
+                        $visitor->visitProperty(
+                            new StaticPropertyMetadata($class, $field, $value),
+                            $value
+                        );
+                        break;
                 }
+                $resultArray = $visitor->endVisitingObject(
+                    $classMetadata,
+                    $value,
+                    []
+                );
+                $object[$field] = array_pop($resultArray);
             } else {
                 $object[$field] = $value;
             }
-        }
-
-        // If the visitor's root is the value of the last field processed, we need to fix the root
-        if (array_key_exists('__replaceme__', $visitor->getRoot())) {
-            $visitor->setRoot($object);
         }
 
         return $object;
@@ -94,22 +96,21 @@ class SObjectHandler implements SubscribingHandlerInterface
         $sobject = new SObject();
 
         $metadata = $context->getMetadataFactory()->getMetadataForClass(SObject::class);
-        $visitor->startVisitingObject($metadata, $sobject, $type, $context);
+        $visitor->startVisitingObject($metadata, $sobject, $type);
 
         foreach ($data as $field => $value) {
             if (is_string($value)
                 && preg_match('/^\d{4}-\d{2}-\d{2}\T\d{2}:\d{2}:\d{2}(\.\d{4})?(\+\d{4}|\Z)$/', $value) != false) {
                 $sobject->$field = $context->getNavigator()->accept(
                     $value,
-                    ['name' => 'DateTime', 'params' => [\DATE_ISO8601, 'UTC']],
-                    $context
+                    ['name' => 'DateTime', 'params' => [\DATE_ISO8601, 'UTC']]
                 );
             } else {
                 $sobject->$field = $value;
             }
         }
 
-        $visitor->endVisitingObject($metadata, $sobject, $type, $context);
+        $visitor->endVisitingObject($metadata, $sobject, $type);
 
         return $sobject;
     }
